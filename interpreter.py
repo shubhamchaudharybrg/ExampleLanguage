939e46352099e2492bd89ddc8e474cdba8e8acf5
Shubham Chaudhary <shubhamchaudharybrg@gmail.com>
2019-08-01 14:22:49 +0530

    Separate Interpreter created.

---
+++ interpreter.py
@@ -0,0 +1,18 @@
+import parsr.py as pr
+
+#Interpreter.
+def interpreter():
+	print('\nExample program to implement functions of BASIC Language.\n')
+#Terminal of the BASIC.
+	while True:
+		ip = input('BASIC >> ')
+		if ip == 'exit':
+			break
+		else:
+			pr.parser(ip)
+			#print(values)
+#       elif not ip:
+#               return 'VoidError : Input is None.'
+if __name__ == '__main__':
+	interpreter()
+#       print(interpreter())
